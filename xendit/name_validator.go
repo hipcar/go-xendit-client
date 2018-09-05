88eb3b7dbb1badf27cb2f75e43a734bf4f48a218
Mychael Go <admin@mychaelgo.com>
2018-09-05 15:55:06 +0700

    name validator

--- xendit/name_validator.go
+++ xendit/name_validator.go
@@ -3,3 +3,27 @@ package xendit
 type NameValidator struct {
 	client *Client
 }
+
+type NameValidatorRequest struct {
+	BankAccountNumber string `json:"bank_account_number"`
+	BankCode          string `json:"bank_code"`
+}
+
+type NameValidatorResponse struct {
+	BankAccountNumber     string `json:"bank_account_number"`
+	BankAccountHolderName string `json:"bank_account_holder_name"`
+	BankCode              string `json:"bank_code"`
+	Reference             string `json:"reference"`
+	Status                string `json:"status"`
+	Updated               string `json:"updated"`
+	Id                    string `json:"id"`
+}
+
+func (c *NameValidator) Validate(body NameValidatorRequest) (NameValidatorResponse, error) {
+	res := new(NameValidatorResponse)
+
+	endpoint := "bank_account_data_requests"
+
+	err := c.client.Request("POST", endpoint, nil, res)
+	return *res, err
+}
